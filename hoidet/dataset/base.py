49fee451b87fd088b8a7de31fe2e6e980f99ca00
jiayuancs <jiayuancs@yeah.net>
2024-04-10 15:47:56 +0800

    feat: add dataset code

---
+++ hoidet/dataset/base.py
@@ -0,0 +1,264 @@
+"""
+数据集的基本信息：
+    - 数据集名称
+    - 存储路径
+    - 数据集划分情况（train/val/test）
+    - 数据集元数据
+"""
+import os
+import pickle
+
+from PIL import Image
+from torch.utils.data import Dataset
+from typing import Any, Callable, List, Optional, Tuple
+
+__all__ = ['DatasetInfo', 'DataDict', 'ImageDataset', 'DataSubset']
+
+
+class DatasetInfo:
+    def __init__(self,
+                 name: str,
+                 data_root: str,
+                 partition_image: dict,
+                 partition_anno: dict,
+                 object_class_num: int,
+                 hoi_class_num: int,
+                 verb_class_num: int):
+        """
+        描述一个数据集
+        Args:
+            name: 数据集名称
+            data_root: 数据集根目录
+            partition_image: 分区名称与图片目录的映射关系
+                例如 {"train2015": "hico_20160224_det/images/train2015"}
+            partition_anno: 分区名称与标注文件的映射关系
+                例如 {"train2015", "instances_train2015.json"}
+            object_class_num: 物体类别数量
+            hoi_class_num: HOI类别数量，即动词与物体的组合数量
+            verb_class_num: 动词类别数量
+        """
+        self._name = name
+        self._data_root = data_root
+        self._partition_image = partition_image
+        self._partition_anno = partition_anno
+        assert self._partition_image.keys() == self._partition_anno.keys()
+        self._partition = list(self._partition_image.keys())
+
+        self._object_class_num = object_class_num
+        self._hoi_class_num = hoi_class_num
+        self._verb_class_num = verb_class_num
+
+    def get_name(self) -> str:
+        return self._name
+
+    def get_root(self) -> str:
+        return self._data_root
+
+    def get_anno_path(self, partition: str) -> str:
+        """
+        获取指定分区的注释文件完整路径
+        Args:
+            partition: 分区名称
+
+        Returns:
+
+        """
+        assert partition in self._partition, \
+            f"Unknown {self._name} partition :{partition}"
+        return os.path.join(self._data_root, self._partition_anno[partition])
+
+    def get_image_path(self, partition: str) -> str:
+        """
+        获取指定分区的图片目录完整路径
+        Args:
+            partition: 分区名称
+
+        Returns:
+
+        """
+        assert partition in self._partition, \
+            f"Unknown {self._name} partition :{partition}"
+        return os.path.join(self._data_root, self._partition_image[partition])
+
+    def __str__(self) -> str:
+        res = (f"Dataset: {self.get_name()}\n"
+               f"\troot: {self.get_root()}\n"
+               f"\tpartition: {self._partition}\n"
+               f"\tobject_class_num: {self._object_class_num}\n"
+               f"\tverb_class_num: {self._verb_class_num}\n"
+               f"\thoi_class_num: {self._hoi_class_num}\n")
+        return res
+
+
+"""
+以下代码摘自: https://github.com/fredzzhang/pocket/blob/master/pocket/data/base.py
+
+Dataset base classes
+
+Fred Zhang <frederic.zhang@anu.edu.au>
+
+The Australian National University
+Australian Centre for Robotic Vision
+"""
+
+
+class DataDict(dict):
+    r"""
+    Data dictionary class. This is a class based on python dict, with
+    augmented utility for loading and saving
+
+    Arguments:
+        input_dict(dict, optional): A Python dictionary
+        kwargs: Keyworded arguments to be stored in the dict
+
+    Example:
+
+        >>> from pocket.data import DataDict
+        >>> person = DataDict()
+        >>> person.is_empty()
+        True
+        >>> person.age = 15
+        >>> person.sex = 'male'
+        >>> person.save('./person.pkl', 'w')
+    """
+
+    def __init__(self, input_dict: Optional[dict] = None, **kwargs) -> None:
+        data_dict = dict() if input_dict is None else input_dict
+        data_dict.update(kwargs)
+        super(DataDict, self).__init__(**data_dict)
+
+    def __getattr__(self, name: str) -> Any:
+        """Get attribute"""
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError(name)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        """Set attribute"""
+        self[name] = value
+
+    def save(self, path: str, mode: str = 'wb', **kwargs) -> None:
+        """Save the dict into a pickle file"""
+        with open(path, mode) as f:
+            pickle.dump(self.copy(), f, **kwargs)
+
+    def load(self, path: str, mode: str = 'rb', **kwargs) -> None:
+        """Load a dict or DataDict from pickle file"""
+        with open(path, mode) as f:
+            data_dict = pickle.load(f, **kwargs)
+        for name in data_dict:
+            self[name] = data_dict[name]
+
+    def is_empty(self) -> bool:
+        return not bool(len(self))
+
+
+class StandardTransform:
+    """https://github.com/pytorch/vision/blob/master/torchvision/datasets/vision.py"""
+
+    def __init__(self, transform: Optional[Callable] = None, target_transform: Optional[Callable] = None) -> None:
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __call__(self, inputs: Any, target: Any) -> Tuple[Any, Any]:
+        if self.transform is not None:
+            inputs = self.transform(inputs)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return inputs, target
+
+    def _format_transform_repr(self, transform: Callable, head: str) -> List[str]:
+        lines = transform.__repr__().splitlines()
+        return (["{}{}".format(head, lines[0])] +
+                ["{}{}".format(" " * len(head), line) for line in lines[1:]])
+
+    def __repr__(self) -> str:
+        body = [self.__class__.__name__]
+        if self.transform is not None:
+            body += self._format_transform_repr(self.transform,
+                                                "Transform: ")
+        if self.target_transform is not None:
+            body += self._format_transform_repr(self.target_transform,
+                                                "Target transform: ")
+
+        return '\n'.join(body)
+
+
+class ImageDataset(Dataset):
+    """
+    Base class for image dataset
+
+    Arguments:
+        root(str): Root directory where images are downloaded to
+        transform(callable, optional): A function/transform that takes in an PIL image
+            and returns a transformed version
+        target_transform(callable, optional): A function/transform that takes in the
+            target and transforms it
+        transforms (callable, optional): A function/transform that takes input sample
+            and its target as entry and returns a transformed version
+    """
+
+    def __init__(self, root: str, transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None,
+                 transforms: Optional[Callable] = None) -> None:
+        self._root = root
+        self._transform = transform
+        self._target_transform = target_transform
+        if transforms is None:
+            self._transforms = StandardTransform(transform, target_transform)
+        elif transform is not None or target_transform is not None:
+            print("WARNING: Argument transforms is given, transform/target_transform are ignored.")
+            self._transforms = transforms
+        else:
+            self._transforms = transforms
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, i):
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
+        """Return the executable string representation"""
+        reprstr = self.__class__.__name__ + '(root=' + repr(self._root)
+        reprstr += ')'
+        # Ignore the optional arguments
+        return reprstr
+
+    def __str__(self) -> str:
+        """Return the readable string representation"""
+        reprstr = 'Dataset: ' + self.__class__.__name__ + '\n'
+        reprstr += '\tNumber of images: {}\n'.format(self.__len__())
+        reprstr += '\tRoot path: {}\n'.format(self._root)
+        return reprstr
+
+    def load_image(self, path: str) -> Image:
+        """Load an image as PIL.Image"""
+        return Image.open(path).convert('RGB')
+
+
+class DataSubset(Dataset):
+    """
+    A subset of data with access to all attributes of original dataset
+
+    Arguments:
+        dataset(Dataset): Original dataset
+        pool(List[int]): The pool of indices for the subset
+    """
+
+    def __init__(self, dataset: Dataset, pool: List[int]) -> None:
+        self.dataset = dataset
+        self.pool = pool
+
+    def __len__(self) -> int:
+        return len(self.pool)
+
+    def __getitem__(self, idx: int) -> Any:
+        return self.dataset[self.pool[idx]]
+
+    def __getattr__(self, key: str) -> Any:
+        if hasattr(self.dataset, key):
+            return getattr(self.dataset, key)
+        else:
+            raise AttributeError("Given dataset has no attribute \'{}\'".format(key))
---
+++ hoidet/dataset/config.py
@@ -0,0 +1,56 @@
+"""
+定义用于模型训练和验证的数据集
+"""
+from .base import DatasetInfo
+
+__all__ = ['HICO_DET_INFO', 'VCOCO_INFO']
+
+# TODO: 这里的数据集data_root参数采用了硬编码，后续可改进
+
+# HICO_DET 数据集
+HICO_DET_INFO = DatasetInfo(
+    name="hicodet",
+    data_root="/workspace/dataset/hicodet",
+    partition_image={
+        "train2015": "hico_20160224_det/images/train2015",
+        "test2015": "hico_20160224_det/images/test2015"
+    },
+    partition_anno={
+        "train2015": "instances_train2015.json",
+        "test2015": "instances_test2015.json"
+    },
+    object_class_num=80,
+    hoi_class_num=600,
+    verb_class_num=117
+)
+
+# VCOCO 数据集
+VCOCO_INFO = DatasetInfo(
+    name="vcoco",
+    data_root="/workspace/dataset/vcoco",
+    partition_image={
+        "train": "mscoco2014/train2014",
+        "val": "mscoco2014/train2014",
+        "trainval": "mscoco2014/train2014",
+        "test": "mscoco2014/val2014"
+    },
+    partition_anno={
+        "train": "instances_vcoco_train.json",
+        "val": "instances_vcoco_val.json",
+        "trainval": "instances_vcoco_trainval.json",
+        "test": "instances_vcoco_test.json"
+    },
+    object_class_num=80,
+    hoi_class_num=-1,  # vcoco 数据集不需要统计该类别信息
+    verb_class_num=24
+)
+
+if __name__ == '__main__':
+    print(HICO_DET_INFO)
+    print(VCOCO_INFO)
+
+    print(HICO_DET_INFO.get_anno_path("test2015"))
+    print(HICO_DET_INFO.get_image_path("test2015"))
+
+    print(VCOCO_INFO.get_anno_path("trainval"))
+    print(VCOCO_INFO.get_image_path("trainval"))
