6c94c1e894055422bae30e36ef249e12f3a862c2
jiayuancs <jiayuancs@yeah.net>
2024-04-16 19:24:01 +0800

    feat: update interface of HICO-DET evaluation program

--- hoidet/metrics/hicodet.py
+++ hoidet/metrics/hicodet.py
@@ -8,13 +8,23 @@ from tqdm import tqdm
 from hoidet.dataset import HICO_DET_INFO, DatasetInfo, HICODet
 from hoidet.utils import BoxPairAssociation, DetectionAPMeter
 
+__all__ = ['HICODetMetric']
+
 
 class HICODetMetric:
     def __init__(self, pred_file_path: str,
                  partition: str = "test2015",
                  dataset_info: DatasetInfo = HICO_DET_INFO,
-                 tp_min_iou: float = 0.5
-                 ):
+                 tp_min_iou: float = 0.5):
+        """
+        Args:
+            pred_file_path: 模型在partition分区上的预测结果文件路径
+            partition: 指定数据集分区
+            dataset_info: 数据集基本信息
+            tp_min_iou: 当人框和物框与真实框IoU的最小值大于tp_min_iou，且预测的HOI类别正确时，被认为是 True Positive
+        """
+        self.tp_min_iou = tp_min_iou
+
         with open(pred_file_path, mode="r", encoding='utf-8') as fd:
             self.preds = json.load(fd)
 
@@ -23,19 +33,19 @@ class HICODetMetric:
             partition=partition
         )
 
+        self.ap = None
+
+    def eval(self):
         # 计算哪些预测结果是 True Positive
-        self.tp_associate = BoxPairAssociation(min_iou=tp_min_iou)
+        tp_associate = BoxPairAssociation(min_iou=self.tp_min_iou)
 
         # 计算 AP
-        self.meter = DetectionAPMeter(
-            600, nproc=1,
+        meter = DetectionAPMeter(
+            self.hicodet.hoi_class_num, nproc=1,
             num_gt=self.hicodet.hoi_instance_num,
             algorithm='11P'
         )
 
-        self.ap = self._eval()
-
-    def _eval(self):
         for pred in tqdm(self.preds):
             image_id = pred['image_id']
             boxes = torch.FloatTensor(pred['boxes'])
@@ -57,15 +67,15 @@ class HICODetMetric:
                 gt_idx = torch.nonzero(torch.LongTensor(target['hoi']) == hoi_idx).squeeze(1)
                 det_idx = torch.nonzero(hoi_class == hoi_idx).squeeze(1)
                 if len(gt_idx):
-                    labels[det_idx] = self.tp_associate(
+                    labels[det_idx] = tp_associate(
                         (gt_boxes_h[gt_idx].view(-1, 4), gt_boxes_o[gt_idx].view(-1, 4)),
                         (boxes_h[det_idx].view(-1, 4), boxes_o[det_idx].view(-1, 4)),
                         scores=hoi_score[det_idx].view(-1)
                     )
 
-            self.meter.append(hoi_score, hoi_class, labels)
+            meter.append(hoi_score, hoi_class, labels)
 
-        return self.meter.eval()
+        self.ap = meter.eval()
 
     def get_ap(self):
         return self.ap
@@ -81,6 +91,6 @@ class HICODetMetric:
 
 
 if __name__ == '__main__':
-    hicodet = HICODetMetric(pred_file_path="../../data/hicodet_pred.json")
-
+    hicodet = HICODetMetric(pred_file_path="/workspace/code/dl_github/HOIDet/data/hicodet_pred.json")
+    hicodet.eval()
     print(f"mAP: {hicodet.get_full_map():0.4f}")
