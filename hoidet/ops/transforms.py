a8b1f6b94f2c1ab1a65e09d635a8d7964a03581b
jiayuancs <jiayuancs@yeah.net>
2024-04-17 21:17:34 +0800

    feat: add code from detr and upt
    detr: https://github.com/facebookresearch/detr
    upt: https://github.com/fredzzhang/upt

--- hoidet/ops/transforms.py
+++ hoidet/ops/transforms.py
@@ -1,172 +1,362 @@
 """
-Useful transforms 
-
-Fred Zhang <frederic.zhang@anu.edu.au>
-
-The Australian National University
-Australian Centre for Robotic Vision
+以下代码来自：
+- https://github.com/fredzzhang/pocket/blob/master/pocket/ops/transforms.py
+- https://github.com/facebookresearch/detr/blob/main/datasets/transforms.py
 """
-
-import torch
 import random
-import torchvision
+import PIL
+import torch
+import torchvision.transforms as T
+import torchvision.transforms.functional as F
+
+from hoidet.utils.box_ops import box_xyxy_to_cxcywh
+from hoidet.utils.misc import interpolate
+
 
 __all__ = [
-    'to_tensor', 'horizontal_flip_boxes', 'horizontal_flip',
-    'ToTensor', 'RandomHorizontalFlip', 'Flatten'
+    'to_list_of_tensor', 'to_tuple_of_tensor', 'to_dict_of_tensor'
 ]
 
 
-def _to_list_of_tensor(x, dtype=None, device=None):
+def to_list_of_tensor(x, dtype=None, device=None):
     return [torch.as_tensor(item, dtype=dtype, device=device) for item in x]
 
 
-def _to_tuple_of_tensor(x, dtype=None, device=None):
+def to_tuple_of_tensor(x, dtype=None, device=None):
     return tuple(torch.as_tensor(item, dtype=dtype, device=device) for item in x)
 
 
-def _to_dict_of_tensor(x, dtype=None, device=None):
+def to_dict_of_tensor(x, dtype=None, device=None):
     return dict([(k, torch.as_tensor(v, dtype=dtype, device=device)) for k, v in x.items()])
 
 
-def to_tensor(x, input_format='tensor', dtype=None, device=None):
-    """Convert input data to tensor based on its format"""
-    if input_format == 'tensor':
-        return torch.as_tensor(x, dtype=dtype, device=device)
-    elif input_format == 'pil':
-        return torchvision.transforms.functional.to_tensor(x).to(
-            dtype=dtype, device=device)
-    elif input_format == 'list':
-        return _to_list_of_tensor(x, dtype=dtype, device=device)
-    elif input_format == 'tuple':
-        return _to_tuple_of_tensor(x, dtype=dtype, device=device)
-    elif input_format == 'dict':
-        return _to_dict_of_tensor(x, dtype=dtype, device=device)
-    else:
-        raise ValueError("Unsupported format {}".format(input_format))
+def crop(image, target, region):
+    cropped_image = F.crop(image, *region)
+
+    target = target.copy()
+    i, j, h, w = region
+
+    # should we do something wrt the original size?
+    target["size"] = torch.tensor([h, w])
+
+    # fields = ["labels", "area"]
+    fields = ["labels", "object"]
+
+    if "boxes" in target:
+        boxes = target["boxes"]
+        max_size = torch.as_tensor([w, h], dtype=torch.float32)
+        cropped_boxes = boxes - torch.as_tensor([j, i, j, i])
+        cropped_boxes = torch.min(cropped_boxes.reshape(-1, 2, 2), max_size)
+        cropped_boxes = cropped_boxes.clamp(min=0)
+        area = (cropped_boxes[:, 1, :] - cropped_boxes[:, 0, :]).prod(dim=1)
+        target["boxes"] = cropped_boxes.reshape(-1, 4)
+        target["area"] = area
+        fields.append("boxes")
+
+    # Crop human and object boxes
+    if "boxes_h" in target:
+        boxes = target["boxes_h"]
+        max_size = torch.as_tensor([w, h], dtype=torch.float32)
+        cropped_boxes = boxes - torch.as_tensor([j, i, j, i])
+        cropped_boxes = torch.min(cropped_boxes.reshape(-1, 2, 2), max_size)
+        cropped_boxes = cropped_boxes.clamp(min=0)
+        area = (cropped_boxes[:, 1, :] - cropped_boxes[:, 0, :]).prod(dim=1)
+        target["boxes_h"] = cropped_boxes.reshape(-1, 4)
+        fields.append("boxes_h")
+    if "boxes_o" in target:
+        boxes = target["boxes_o"]
+        max_size = torch.as_tensor([w, h], dtype=torch.float32)
+        cropped_boxes = boxes - torch.as_tensor([j, i, j, i])
+        cropped_boxes = torch.min(cropped_boxes.reshape(-1, 2, 2), max_size)
+        cropped_boxes = cropped_boxes.clamp(min=0)
+        area = (cropped_boxes[:, 1, :] - cropped_boxes[:, 0, :]).prod(dim=1)
+        target["boxes_o"] = cropped_boxes.reshape(-1, 4)
+        fields.append("boxes_o")
+
+    if "masks" in target:
+        # FIXME should we update the area here if there are no boxes?
+        target['masks'] = target['masks'][:, i:i + h, j:j + w]
+        fields.append("masks")
+
+    # remove elements for which the boxes or masks that have zero area
+    if "boxes" in target or "masks" in target or "boxes_h" in target or "boxes_o" in target:
+        # favor boxes selection when defining which elements to keep
+        # this is compatible with previous implementation
+        if "boxes" in target:
+            cropped_boxes = target['boxes'].reshape(-1, 2, 2)
+            keep = torch.all(cropped_boxes[:, 1, :] > cropped_boxes[:, 0, :], dim=1)
+        elif "masks" in target:
+            keep = target['masks'].flatten(1).any(1)
+        else:
+            cropped_bh = target['boxes_h'].reshape(-1, 2, 2)
+            cropped_bo = target['boxes_o'].reshape(-1, 2, 2)
+            keep = torch.logical_and(
+                torch.all(cropped_bh[:, 1, :] > cropped_bo[:, 0, :], dim=1),
+                torch.all(cropped_bo[:, 1, :] > cropped_bo[:, 0, :], dim=1)
+            )
+
+        for field in fields:
+            target[field] = target[field][keep]
+
+    return cropped_image, target
 
 
-def horizontal_flip_boxes(w, boxes, encoding='coords'):
+def hflip(image, target):
+    flipped_image = F.hflip(image)
+
+    w, h = image.size
+
+    target = target.copy()
+    if "boxes" in target:
+        boxes = target["boxes"]
+        boxes = boxes[:, [2, 1, 0, 3]] * torch.as_tensor([-1, 1, -1, 1]) + torch.as_tensor([w, 0, w, 0])
+        target["boxes"] = boxes
+
+    # Flip human and object boxes
+    if "boxes_h" in target:
+        boxes = target["boxes_h"]
+        boxes = boxes[:, [2, 1, 0, 3]] * torch.as_tensor([-1, 1, -1, 1]) + torch.as_tensor([w, 0, w, 0])
+        target["boxes_h"] = boxes
+    if "boxes_o" in target:
+        boxes = target["boxes_o"]
+        boxes = boxes[:, [2, 1, 0, 3]] * torch.as_tensor([-1, 1, -1, 1]) + torch.as_tensor([w, 0, w, 0])
+        target["boxes_o"] = boxes
+
+    if "masks" in target:
+        target['masks'] = target['masks'].flip(-1)
+
+    return flipped_image, target
+
+
+def resize(image, target, size, max_size=None):
+    # size can be min_size (scalar) or (w, h) tuple
+
+    def get_size_with_aspect_ratio(image_size, size, max_size=None):
+        w, h = image_size
+        if max_size is not None:
+            min_original_size = float(min((w, h)))
+            max_original_size = float(max((w, h)))
+            if max_original_size / min_original_size * size > max_size:
+                size = int(round(max_size * min_original_size / max_original_size))
+
+        if (w <= h and w == size) or (h <= w and h == size):
+            return (h, w)
+
+        if w < h:
+            ow = size
+            oh = int(size * h / w)
+        else:
+            oh = size
+            ow = int(size * w / h)
+
+        return (oh, ow)
+
+    def get_size(image_size, size, max_size=None):
+        if isinstance(size, (list, tuple)):
+            return size[::-1]
+        else:
+            return get_size_with_aspect_ratio(image_size, size, max_size)
+
+    size = get_size(image.size, size, max_size)
+    rescaled_image = F.resize(image, size)
+
+    if target is None:
+        return rescaled_image, None
+
+    ratios = tuple(float(s) / float(s_orig) for s, s_orig in zip(rescaled_image.size, image.size))
+    ratio_width, ratio_height = ratios
+
+    target = target.copy()
+    if "boxes" in target:
+        boxes = target["boxes"]
+        scaled_boxes = boxes * torch.as_tensor([ratio_width, ratio_height, ratio_width, ratio_height])
+        target["boxes"] = scaled_boxes
+
+    # Resize human and object boxes
+    if "boxes_h" in target:
+        boxes = target["boxes_h"]
+        scaled_boxes = boxes * torch.as_tensor([ratio_width, ratio_height, ratio_width, ratio_height])
+        target["boxes_h"] = scaled_boxes
+    if "boxes_o" in target:
+        boxes = target["boxes_o"]
+        scaled_boxes = boxes * torch.as_tensor([ratio_width, ratio_height, ratio_width, ratio_height])
+        target["boxes_o"] = scaled_boxes
+
+    if "area" in target:
+        area = target["area"]
+        scaled_area = area * (ratio_width * ratio_height)
+        target["area"] = scaled_area
+
+    h, w = size
+    target["size"] = torch.tensor([h, w])
+
+    if "masks" in target:
+        target['masks'] = interpolate(
+            target['masks'][:, None].float(), size, mode="nearest")[:, 0] > 0.5
+
+    return rescaled_image, target
+
+
+def pad(image, target, padding):
+    # assumes that we only pad on the bottom right corners
+    padded_image = F.pad(image, (0, 0, padding[0], padding[1]))
+    if target is None:
+        return padded_image, None
+    target = target.copy()
+    # should we do something wrt the original size?
+    target["size"] = torch.tensor(padded_image.size[::-1])
+    if "masks" in target:
+        target['masks'] = torch.nn.functional.pad(target['masks'], (0, padding[0], 0, padding[1]))
+    return padded_image, target
+
+
+class RandomCrop(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, img, target):
+        region = T.RandomCrop.get_params(img, self.size)
+        return crop(img, target, region)
+
+
+class RandomSizeCrop(object):
+    def __init__(self, min_size: int, max_size: int):
+        self.min_size = min_size
+        self.max_size = max_size
+
+    def __call__(self, img: PIL.Image.Image, target: dict):
+        w = random.randint(self.min_size, min(img.width, self.max_size))
+        h = random.randint(self.min_size, min(img.height, self.max_size))
+        region = T.RandomCrop.get_params(img, [h, w])
+        return crop(img, target, region)
+
+
+class CenterCrop(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, img, target):
+        image_width, image_height = img.size
+        crop_height, crop_width = self.size
+        crop_top = int(round((image_height - crop_height) / 2.))
+        crop_left = int(round((image_width - crop_width) / 2.))
+        return crop(img, target, (crop_top, crop_left, crop_height, crop_width))
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, img, target):
+        if random.random() < self.p:
+            return hflip(img, target)
+        return img, target
+
+
+class RandomResize(object):
+    def __init__(self, sizes, max_size=None):
+        assert isinstance(sizes, (list, tuple))
+        self.sizes = sizes
+        self.max_size = max_size
+
+    def __call__(self, img, target=None):
+        size = random.choice(self.sizes)
+        return resize(img, target, size, self.max_size)
+
+
+class RandomPad(object):
+    def __init__(self, max_pad):
+        self.max_pad = max_pad
+
+    def __call__(self, img, target):
+        pad_x = random.randint(0, self.max_pad)
+        pad_y = random.randint(0, self.max_pad)
+        return pad(img, target, (pad_x, pad_y))
+
+
+class RandomSelect(object):
     """
-    Horizontally flip bounding boxes with respect to an image.
-
-    Parameters
-    ----------
-    w : int
-        Width of the image, to which given boxes belong.
-    boxes : Tensor
-        Bounding box tensors (N, 4) in the format x1, y1, x2, y2.
-    encoding : str, optional
-        Encoding method of the bounding boxes. Choose between 'coords' and 'pixel'.
-        Using 'coords' implies the boxes are encoded as coordinates, which mean the
-        values are no more than the dimensions of the image. Using 'pixel' implies
-        the boxes are encoded as pixel indices, meaning the largest values are no
-        more than the dimensions of the image minus one (Default is 'coords').
-
-    Returns
-    -------
-    boxes: Tensor
-        Flipped bounding box tensors (N, 4) in the same format and encoding.
+    Randomly selects between transforms1 and transforms2,
+    with probability p for transforms1 and (1 - p) for transforms2
     """
-    boxes = boxes.clone()
-    if encoding == 'coords':
-        x_min = w - boxes[:, 2]
-        x_max = w - boxes[:, 0]
-        boxes[:, 0] = torch.clamp(x_min, 0, w)
-        boxes[:, 2] = torch.clamp(x_max, 0, w)
-    elif encoding == 'pixel':
-        x_min = w - boxes[:, 2] - 1
-        x_max = w - boxes[:, 0] - 1
-        boxes[:, 0] = torch.clamp(x_min, 0, w - 1)
-        boxes[:, 2] = torch.clamp(x_max, 0, w - 1)
-    else:
-        raise ValueError("Unknown box encoding \'{}\'".format(encoding))
-    return boxes
+
+    def __init__(self, transforms1, transforms2, p=0.5):
+        self.transforms1 = transforms1
+        self.transforms2 = transforms2
+        self.p = p
+
+    def __call__(self, img, target):
+        if random.random() < self.p:
+            return self.transforms1(img, target)
+        return self.transforms2(img, target)
 
 
-def horizontal_flip(image, boxes=None, encoding='coords'):
-    """
-    Horizontally flip an image and its associated bounding boxes if any.
-
-    Parameters
-    ----------
-    image : PIL.Image
-        Input image.
-    boxes : Tensor, optional
-        Bounding box tensors (N, 4) in the format x1, y1, x2, y2. When left as None
-        only the image will be flipped (Default is None).
-    encoding : str, optional
-        Encoding method of the bounding boxes. Choose between 'coords' and 'pixel'.
-        Using 'coords' implies the boxes are encoded as coordinates, which mean the
-        values are no more than the dimensions of the image. Using 'pixel' implies
-        the boxes are encoded as pixel indices, meaning the largest values are no
-        more than the dimensions of the image minus one (Default is 'coords').
-
-    Returns
-    -------
-    image: PIL.Image
-        Input image horizontally flipped.
-    boxes: Tensor
-        Flipped bounding box tensors (N, 4) in the same format and encoding. This will
-        only be returned when the input argument `boxes` is not None.
-    """
-    image = torchvision.transforms.functional.hflip(image)
-    w, _ = image.size
-    if boxes is None:
-        return image
-    else:
-        boxes = horizontal_flip_boxes(w, boxes, encoding=encoding)
-    return image, boxes
+class ToTensor(object):
+    def __call__(self, img, target):
+        return F.to_tensor(img), target
 
 
-class ToTensor:
-    """Convert to tensor"""
+class RandomErasing(object):
 
-    def __init__(self, input_format='tensor', dtype=None, device=None):
-        self.input_format = input_format
-        self.dtype = dtype
-        self.device = device
+    def __init__(self, *args, **kwargs):
+        self.eraser = T.RandomErasing(*args, **kwargs)
 
-    def __call__(self, x):
-        return to_tensor(x,
-                         input_format=self.input_format,
-                         dtype=self.dtype,
-                         device=self.device
-                         )
+    def __call__(self, img, target):
+        return self.eraser(img), target
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, image, target=None):
+        image = F.normalize(image, mean=self.mean, std=self.std)
+        if target is None:
+            return image, None
+        target = target.copy()
+        h, w = image.shape[-2:]
+        if "boxes" in target:
+            boxes = target["boxes"]
+            boxes = box_xyxy_to_cxcywh(boxes)
+            boxes = boxes / torch.tensor([w, h, w, h], dtype=torch.float32)
+            target["boxes"] = boxes
+
+        # Normalise human and object boxes
+        if "boxes_h" in target:
+            boxes = target["boxes_h"]
+            boxes = box_xyxy_to_cxcywh(boxes)
+            boxes = boxes / torch.tensor([w, h, w, h], dtype=torch.float32)
+            target["boxes_h"] = boxes
+        if "boxes_o" in target:
+            boxes = target["boxes_o"]
+            boxes = box_xyxy_to_cxcywh(boxes)
+            boxes = boxes / torch.tensor([w, h, w, h], dtype=torch.float32)
+            target["boxes_o"] = boxes
+        return image, target
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image, target = t(image, target)
+        return image, target
 
     def __repr__(self):
-        reprstr = self.__class__.__name__ + '('
-        reprstr += 'input_format={}'.format(repr(self.input_format))
-        reprstr += ', dtype='
-        reprstr += repr(self.dtype)
-        reprstr += ', device='
-        reprstr += repr(self.device)
-        reprstr += ')'
-        return reprstr
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            format_string += "    {0}".format(t)
+        format_string += "\n)"
+        return format_string
 
 
-class RandomHorizontalFlip:
-    """Horizontally flip an image and its associated bounding boxes if any"""
+class ColorJitter(object):
+    def __init__(self, brightness=0, contrast=0, saturatio=0, hue=0):
+        self.color_jitter = T.ColorJitter(brightness, contrast, saturatio, hue)
 
-    def __init__(self, prob=0.5):
-        self.prob = prob
-
-    def __call__(self, image, boxes=None, encoding='coords'):
-        prob = random.random()
-        if prob < self.prob and boxes is None:
-            return image
-        elif prob < self.prob and boxes is not None:
-            return image, boxes
-        return horizontal_flip(image, boxes, encoding)
-
-
-class Flatten(torch.nn.Module):
-    """Flatten a tensor"""
-
-    def __init__(self, start_dim=0, end_dim=-1):
-        super().__init__()
-        self.start_dim = start_dim
-        self.end_dim = end_dim
-
-    def forward(self, x):
-        return x.flatten(start_dim=self.start_dim, end_dim=self.end_dim)
+    def __call__(self, img, target):
+        return self.color_jitter(img), target
